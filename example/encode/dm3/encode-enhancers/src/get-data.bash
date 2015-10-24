d8acc5744d5f1d7f7673f34418ac2208492f59e9
daler <dalerr@niddk.nih.gov>
2015-10-24 10:50:50 -0400

    rm example data
    (since it's being move to another repo)

--- example/encode/dm3/encode-enhancers/src/get-data.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-(
-    cd $HERE/../raw-data
-    wget http://compbio.med.harvard.edu/modencode/webpage/enh_calls_final/comparative_enhancer_calls.tar.gz
-    tar -xzvf comparative_enhancer_calls.tar.gz
-    rm comparative_enhancer_calls.tar.gz
-)
--- example/encode/dm3/encode-hic-domains/src/get-data.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-(
-   cd $HERE/../raw-data
-
-   # Hi-C domains in embryo
-   wget http://compbio.med.harvard.edu/modencode/webpage/hic/HiC_EL.bed
-)
-
