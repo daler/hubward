9a3b3716cf54c7deded5397f026cea29c60e8adc
daler <dalerr@niddk.nih.gov>
2015-10-26 17:15:05 -0400

    improve skeleton templates

--- resources/metadata_builder_template.py
+++ resources/metadata_builder_template.py
@@ -1,62 +1,97 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+
 import yaml
 import os
 
-# Study configuration
 d = {
-    'study':
+    # Bibliographic information about the study
+    'study': {
+        # Label for this study. The sanitized version of this (non-alphanumeric
+        # characters stripped) should be unique among a group.
+        'label': 'hubward example',
 
-        # This will be included in HTML documentation
-        'reference': "CHANGE ME (copy from citation manager)",
+        # Optional short label for this study. It will be shown in blue link
+        # text in the browser. If not provided, the value of 'label' will be
+        # used.
+        'short_label': 'Hubward example hub',
 
-        # If provided, HTML documentation will link to this PubMed entry
-        'PMID': None,
+        # Long-form label for this study. This is shown as a title on the
+        # study's configuration page. If not provided, the value of
+        # 'short_label' will be used.
+        'long_label': 'Example track hub for demonstrating hubward',
 
-        # 
-        'description': "CHANGE ME (brief study description)",
+        # Optional reference to study, often copied from reference manager.
+        # This will be included in the HTML documentation for the study's page
+        # in the genome browser.
+        'reference': 'See https://github.com/daler/hubward',
 
-        # Label 
-        'label': "CHANGE ME (unique label)",
-        'processing': open('README').read()
+        # Optional PubMed ID, e.g. "0001110" or "PMID:001110". If it is
+        # present, HTML documentation will create a link to this PubMed entry.
+        'PMID': "",
+
+        # Description of the study to be included in the HTML documentation.
+        # This is assumed to be in ReStructured Text format, which is converted
+        # to HTML. If you're using a metadata-builder.py script, a common
+        # pattern is to include the contents of a README file in this field.
+        # This is by far the most verbose section, and is used to describe
+        # where the data came from as well as any processing that was needed to
+        # convert into a format supported by the UCSC Genome Browser.
+        'description': open('README.rst').read(),
+    }
 }
 
 FILENAMES = [
-    'suhw_kc.bed',
-    'shep_bg3.bed',
+    ('a.dat',  'example interval data'),
+    ('b.dat',  'example signal data'),
 ]
 
-d['data'] = []
-for fn in fns:
+d['tracks'] = []
+
+
+# This demo script simply does a copy operation, so we can use it for both
+# the bigbed and bigwig example data.
+script = os.path.join('src', 'dat2bigbed.sh')
+
+for fn, description in FILENAMES:
     original = os.path.join('raw-data', fn)
-    processed = os.path.join('processed-data/bigbed/', fn.replace('bed', 'bigbed'))
-    script = os.path.join('src', 'process.py')
+    if fn.startswith('a'):
+        kind = 'bigbed'
+        label = 'example features for "a"'
 
-    # example conversion of filenams to description/label fields
-    ab, ct = fn.replace('.bed', '').split('_')
-    description = '{ab} ChIP-chip in {ct} cells'.format(**locals())
-    label = '{ab}-{ct}'.format(**locals())
+        # See http://genome.ucsc.edu/goldenPath/help/trackDb/trackDbHub.html
+        trackinfo = {
+            'tracktype': 'bed6',
+            'visibility': 'dense',
+            'color': '255,0,0',
+        }
+    elif fn.startswith('b'):
+        kind = 'bigwig'
+        label = 'example signal for "b"'
+        trackinfo = {
+            'tracktype': 'bigwig',
+            'visibility': 'full',
+            'viewLimts': '0:5',
+        }
 
-    # See http://genome.ucsc.edu/goldenPath/help/trackDb/trackDbHub.html
-    trackinfo = {
-        'visibility': 'dense',
-        'itemRgb': '"on"',
-        'minGrayLevel': 3,
+    processed = os.path.join('processed-data/', fn.replace('dat', kind))
+    source = {
+        'url': ('https://raw.githubusercontent.com/daler/hubward'
+                '/master/resources/{0}'.format(fn)),
+        'fn': fn,
     }
 
-    d['data'].append(
+    d['tracks'].append(
         dict(
             original=original,
             processed=processed,
             script=script,
             description=description,
-            label=label,
-            genome='dm3',
-            url='url to supplemental data',
-            type='bigbed',
+            short_label=label,
+            genome='hg19',
+            source=source,
+            type=kind,
             trackinfo=trackinfo,
         )
     )
 
-
-pyaml.dump(d, open('metadata.yaml', 'w'), vspacing=[1,1])
+yaml.dump(d, open('metadata.yaml', 'w'), default_flow_style=False)
