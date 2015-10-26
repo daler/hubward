3fbfbbeb8247c65e14b6d8baf84be2505fb227ab
daler <dalerr@niddk.nih.gov>
2015-10-26 12:05:44 -0400

    add bulk liftover test and upload test

--- docker-test.sh
+++ docker-test.sh
@@ -17,9 +17,35 @@ ssh-keygen -f /root/.ssh/id_rsa -N ""
 ssh-keyscan -H localhost >> /root/.ssh/known_hosts
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
+
+
 # Finally run the example
 git clone https://github.com/daler/hubward-studies.git
 cd hubward-studies
 hubward process yip-2012
 hubward process lieberman-2009
 
+hubward liftover \
+    --from_assembly hg18 \
+    --to_assembly hg19 \
+    lieberman-2009 \
+    lieberman-2009-hg19
+
+cat > group.yaml << EOF
+name: "hubward-example"
+genome: "hg19"
+short_label: "Hubward example"
+long_label: "Hubward example tracks"
+hub_url: "http://example.com/hubs/example.hub.txt"
+email: "dalerr@niddk.nih.gov"
+
+
+
+studies:
+    - "yip-2012"
+    - "lieberman-2009-hg19"
+EOF
+
+
+hubward upload --host localhost --user root --hub_remote $(pwd)/uploaded_hub group.yaml
+rm group.yaml
